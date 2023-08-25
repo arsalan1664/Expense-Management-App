eefede42bfe5fc02f6e3b67f57df3e68561b56ca
Arsalan Ali <arsalan1664@gmail.com>
2023-08-25 10:44:55 +0000

    1 hour

---
+++ Server/Client/src/components/layouts/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light p-4'>
+        <h6 className='text-center'>
+            All right reserve &copy;  Arsalan Ali
+        </h6>
+    </div>
+  )
+}
+
+export default Footer
